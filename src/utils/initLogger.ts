8c0a9df57fba7631d3652be784478d9fccc59499
Liam <34554573+RoarkGit@users.noreply.github.com>
2022-08-05 19:22:59 -0400

    Added cleaner log format for console messages.

--- src/utils/initLogger.ts
+++ src/utils/initLogger.ts
@@ -4,12 +4,19 @@ import { createLogger, format, transports } from "winston";
 import TransportStream from "winston-transport";
 import LokiTransport from "winston-loki";
 
+const logFormat = format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+    format.printf((log) => {
+        return `${log.timestamp} [${log.level}]: ${log.message}`;
+    })
+);
+
 export const initLogger = async (strago: Strago): Promise<void> => {
     if (strago.logger) return;
 
     const winstonTransports: TransportStream[] = [
         new transports.Console({
-            format: format.combine(format.timestamp(), format.simple(), format.colorize())
+            format: format.combine(logFormat, format.colorize())
         })
     ];
 
@@ -17,7 +24,6 @@ export const initLogger = async (strago: Strago): Promise<void> => {
         winstonTransports.push(new LokiTransport({
             host: strago.config.loggerUri,
             labels: { app: "Strago" },
-            json: true,
             format: format.json(),
             onConnectionError: (err) => console.error(err),
         }));
