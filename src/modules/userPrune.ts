11bd331298a7f7897c2013142067f519efb888de
Liam <34554573+RoarkGit@users.noreply.github.com>
2022-09-30 13:29:54 -0400

    Fixed user prune to only prune in home guild.

--- src/modules/userPrune.ts
+++ src/modules/userPrune.ts
@@ -4,15 +4,15 @@ import { Strago } from '../interfaces/Strago'
  * Prunes all users who have not verified and joined more than ten minutes ago.
  */
 export const userPrune = async (strago: Strago): Promise<void> => {
-  const guilds = strago.guilds.cache
+  // Only prune from home guild.
+  const guild = strago.guilds.cache.get(strago.config.homeGuildId)
+  if (guild === undefined) return
   const now = Date.now()
-  await Promise.all(guilds.map(async (g) => {
-    const tenMinutesMs = 1000 * 60 * 10
-    const members = await g.members.fetch()
-    const toPrune = members.filter(m => m.roles.cache.every(r => r.name !== 'Verified') && m.joinedTimestamp !== null && (now - m.joinedTimestamp > tenMinutesMs))
-    await Promise.all(toPrune.map(async m => {
-      strago.logger.info(`Removing unverified user: ${m.user.username}`)
-      await m.kick('Verification time exceeded ten minutes.').catch(error => strago.logger.error(error))
-    }))
+  const tenMinutesMs = 1000 * 60 * 10
+  const members = await guild.members.fetch()
+  const toPrune = members.filter(m => m.roles.cache.every(r => r.name !== 'Verified') && m.joinedTimestamp !== null && (now - m.joinedTimestamp > tenMinutesMs))
+  await Promise.all(toPrune.map(async m => {
+    strago.logger.info(`Removing unverified user: ${m.user.username}`)
+    await m.kick('Verification time exceeded ten minutes.').catch(error => strago.logger.error(error))
   }))
 }
