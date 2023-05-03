21c128fe18113f6bedb495a2659c7db10bfaa6a8
Liam <34554573+RoarkGit@users.noreply.github.com>
2023-05-03 12:32:35 -0400

    Changed timeout to 1 hour.

--- src/events/checkLFGSpam.ts
+++ src/events/checkLFGSpam.ts
@@ -3,6 +3,7 @@ import { Strago } from '../interfaces/Strago'
 import { ChannelType, Message } from 'discord.js'
 
 const slowmodeDuration = 5 * 60 * 1000
+const timeoutDuration = 60 * 60 * 1000
 
 /**
  * Times out users who ping roles in multiple LFG channels in quick succession.
@@ -18,7 +19,7 @@ export const checkLFGSpam = async (message: Message, strago: Strago): Promise<vo
     if (strago.lfgSpamSet.has(member.id)) {
       await message.reply('Pinging roles in multiple LFG channels spams people who might be in those channels. You have been timed out for one hour.\n' +
                           '_Posting_ in multiple channels is fine as long as you do _not_ ping in multiple channels.')
-      await message.member.timeout(300, 'Spamming LFG channels with pings.')
+      await message.member.timeout(timeoutDuration, 'Spamming LFG channels with pings.')
       if (member.nickname !== null) {
         strago.logger.info(`Timed out ${member.nickname} for spamming LFG.`)
       }
