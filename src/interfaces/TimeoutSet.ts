403629df415b5dc88e8dd9e24396188ce4442702
Liam <34554573+RoarkGit@users.noreply.github.com>
2023-07-05 12:32:32 -0400

    Fixing lint issues.

--- src/interfaces/TimeoutSet.ts
+++ src/interfaces/TimeoutSet.ts
@@ -2,28 +2,28 @@
  * Set used for rate limiting commands. Elements are removed a specified time after being added.
  */
 export class TimeoutSet extends Set<string> {
-    timeoutSeconds: number
+  timeoutSeconds: number
 
-    constructor(timeoutSeconds: number) {
-        super()
-        this.timeoutSeconds = timeoutSeconds * 1000
-    }
+  constructor (timeoutSeconds: number) {
+    super()
+    this.timeoutSeconds = timeoutSeconds * 1000
+  }
 
-    /**
+  /**
      * Checks if an element exists in the set.
      * @param element The value to test for presence in the set.
      * @returns Returns true if 'element' was found in the set.
      */
-    public has(element: string): boolean {
-        return super.has(element)
-    }
+  public has (element: string): boolean {
+    return super.has(element)
+  }
 
-    /**
+  /**
      * Inserts an element into the set, then sets a timer to remove it when the rate limit expires.
      * @param element The element being inserted.
      */
-    public add(element: string): this {
-        setTimeout(() => this.delete(element), this.timeoutSeconds)
-        return super.add(element)
-    }
+  public add (element: string): this {
+    setTimeout(() => this.delete(element), this.timeoutSeconds)
+    return super.add(element)
+  }
 }
