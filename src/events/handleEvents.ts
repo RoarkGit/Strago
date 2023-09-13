b65219440d66cb1b6662bfe33e4dcaa234ecc5f1
RoarkGit <34554573+RoarkGit@users.noreply.github.com>
2023-09-12 20:24:36 -0400

    Added filler message check.

--- src/events/handleEvents.ts
+++ src/events/handleEvents.ts
@@ -22,6 +22,23 @@ export const handleEvents = (strago: Strago): void => {
   strago.on('messageCreate', async (message) => {
     await checkLFGSpam(message, strago)
     await publishAnnouncement(message, strago)
+
+    // TODO: Remove after deleting filler roles.
+    if (
+      message.channel.isTextBased() &&
+      !message.channel.isDMBased() &&
+      message.channel.name.endsWith('lfg') &&
+      message.mentions.roles.hasAny(
+        '977725242694848562',
+        '991144480369557696',
+        '991144675824123944',
+      )
+    ) {
+      await message.reply({
+        content:
+          "It looks like you're looking for a fill! Try using `/fill find` instead. Filler roles are going to be removed in the future, see here for more info: https://discord.com/channels/762797677133561887/1150923406380900382",
+      })
+    }
   })
 
   strago.on('messageDelete', async (message) => {
