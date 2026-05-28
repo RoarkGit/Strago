f622da05a0abc17fb59cbd29d455b73412ab0297
RoarkGit <34554573+RoarkGit@users.noreply.github.com>
2026-05-27 20:05:21 -0400

    chore: use NODE_ENV=prod for production path detection

--- src/utils/paths.ts
+++ src/utils/paths.ts
@@ -1,7 +1,7 @@
 import { join } from 'path'
 
 const getDataDir = () =>
-  join(process.cwd(), process.env.NODE_ENV === 'production' ? 'data/blue-mage-data' : 'src/data/blue-mage-data')
+  join(process.cwd(), process.env.NODE_ENV === 'prod' ? 'data/blue-mage-data' : 'src/data/blue-mage-data')
 
 export const PATHS = {
   spellData: () => join(getDataDir(), 'spell.yaml'),
